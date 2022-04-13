d1aac8da5e053d39d12e442b0a8342aa308827da
Samuel Preston <samp.reston@outlook.com>
2022-04-14 00:20:51 +0100

    Added body and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Body from './components/Body';
 import './styles/App.css'
 
 function App() {
   return (
     <>
-      
+      <Body />
     </>
   );
 }
---
+++ src/components/Body.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Body.css'
+
+import Footer from './Footer'
+
+function Body() {
+  return (
+    <div className='body x-center'>
+      <Footer />
+    </div>
+  )
+}
+
+export default Body
