0b0d52d6b1f1608c5fe7a0aa2f023eeab449c825
Samuel Preston <samp.reston@outlook.com>
2022-04-15 01:24:07 +0100

    Added Socials to Menu

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import '../styles/Socials.css'
+
+import devToLogo from '../assets/BxlDevTo.svg'
+import github from '../assets/BxlGithub.svg'
+import linkedin from '../assets/BxlLinkedin.svg'
 
 function Socials() {
   return (
-    <>
-      
-    </>
+    <nav className='socials x-center'>
+      <img src={devToLogo} alt="Dev.To Logo" />
+      <img src={github} alt="Dev.To Logo" />
+      <img src={linkedin} alt="Dev.To Logo" />
+    </nav>
   )
 }
 
