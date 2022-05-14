a909a587ffe894324e2d4d3176885add07e07123
Samuel Preston <samp.reston@outlook.com>
2022-05-14 17:56:54 +0100

    change to left content

--- src/components/MySkills.tsx
+++ src/components/MySkills.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MySkills() {
   return (
-    <div id="myskills" className='content right-content'>
+    <div id="myskills" className='content left-content'>
       <h2>My Skills</h2>
       <p>
         I'm self-taught through YouTube and online courses. I'm a logical thinker and engineer at heart so I methodically approach problems.
@@ -10,13 +10,13 @@ function MySkills() {
         I believe in Clean Code and Test Driven Development to create matainable software to benefit users and developers.
         <br /> <br />
         I have experience with: <br />
-          &#8226; ReactJS <br />
-          &#8226; TypeScript <br />
-          &#8226; TailwindCSS <br />
-          &#8226; NodeJS <br />
-          &#8226; Python <br />
-          &#8226; MySQL <br />
-          &#8226; GitLab/GitHub <br />
+        &#8226; ReactJS <br />
+        &#8226; TypeScript <br />
+        &#8226; TailwindCSS <br />
+        &#8226; NodeJS <br />
+        &#8226; Python <br />
+        &#8226; MySQL <br />
+        &#8226; GitLab/GitHub <br />
       </p>
     </div>
   )
