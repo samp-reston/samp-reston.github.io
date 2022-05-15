642c58f96aad57832080dee79b137576d6e0043b
Samuel Preston <samp.reston@outlook.com>
2022-05-15 01:42:30 +0100

    Added portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { fetchSection } from '../api/contentAPI'
 
 function Portfolio() {
+  const section = fetchSection("Portfolio")
+  const { sectionName, content } = section
+
+  const renderContent = () => {
+    type Project = {
+      title: string;
+      summary: string;
+      description: string[]
+    }
+    return content.map((project: Project) => (
+      <>
+        <h4>
+          {project.title}
+        </h4>
+        <section key={uuidv4()}>
+          <div className='left-half'>
+            <p>{project.summary}</p>
+          </div>
+          <div className='right-half'>
+            <h4>
+              Skills and Tools Used:
+            </h4>
+            {project.description.map((description: string) => <li key={uuidv4()}>{description}</li>)}
+          </div>
+        </section>
+      </>
+    ))
+  }
+
   return (
-    <div id="projects" className='content left-content'>
-      <h2>Portfolio</h2>
-      {/* Add Project Components */}
+    <div id="projects" className='content'>
+      <h2>{sectionName}</h2>
+      {renderContent()}
     </div>
   )
 }
